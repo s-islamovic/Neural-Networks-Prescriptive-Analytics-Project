59660ccd7d3be0b368073ed91dd7746dfee68be3
Semin Islamovic <seminxd13@gmail.com>
2025-12-12 23:43:34 +0000

    Delete src/evaluate.py

--- src/evaluate.py
+++
@@ -1,9 +0,0 @@
-from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
-
-def evaluate_model(model, X_test, y_test):
-    preds = model.predict(X_test)
-    preds = (preds > 0.5).astype(int)
-
-    print("\nCONFUSION MATRIX:\n", confusion_matrix(y_test, preds))
-    print("\nREPORT:\n", classification_report(y_test, preds))
-    print("AUC:", roc_auc_score(y_test, preds))
